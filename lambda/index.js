4129f0891d6c1a383235a5e35825d3cd7e2c5283
신주현 <shinjhhp5@gmail.com>
2020-04-14 02:03:49 +0900

    update lambda fix handler Key

--- lambda/index.js
+++ lambda/index.js
@@ -5,7 +5,9 @@ const S3 = new AWS.S3({ region: 'ap-northeast-2' });
 
 exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name;
-  const Key = event.Records[0].s3.object.key;
+  const Key = decodeURIComponent(
+    event.Records[0].s3.object.key.replace(/\+/g, ' '),
+  );
   const filename = Key.split('/')[Key.split('/').length - 1];
   const ext = Key.split('.')[Key.split('.').length - 1];
 
