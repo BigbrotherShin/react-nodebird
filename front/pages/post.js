642a0825abd080b98866647ca96fa08579472c15
신주현 <shinjhhp5@gmail.com>
2020-04-14 00:33:11 +0900

    add lambda

--- front/pages/post.js
+++ front/pages/post.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { LOAD_POST_REQUEST } from '../reducers/post';
